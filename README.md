7c78d2f587e8267a4fd6f2972a54a0a28fbdfec9
Matthew Gallant <matthew@gangles.ca>
2021-05-16 13:06:52 -0700

    Completed project for upload to Github

---
+++ README.md
@@ -0,0 +1,19 @@
+Climate Clock Bot
+====================================
+
+**This bot is deployed at: [https://twitter.com/ClimateClockBot](https://twitter.com/ClimateClockBot)**
+
+A bot that tweets daily updates about worldwide CO₂ emissions relative to the [1.5°C carbon budget](https://www.ipcc.ch/sr15/chapter/spm/#article-spm-c) outlined by the IPCC.
+
+A breakdown of the creative intent of this bot is posted on [my blog](http://gangles.ca/2021/05/15/climate-clock-bot/).
+
+Methodology
+====================================
+
+The model and data for this bot are provided by the [Climate Clock](https://climateclock.world/) project’s [API](https://climate-clock.gitbook.io/climate-clock-docs/climate-clock-api).
+
+![Screenshot of a tweet from @ClimateClockBot.](sampletweet.png)
+
+Their countdown time is based on [two pieces of data](https://www.ipcc.ch/sr15/chapter/spm/#article-spm-c) provided by the IPCC. As of 2018, the world has a remaining budget of 420 gigatonnes of CO₂ for a 66% probability of remaining under +1.5˚C global average temperature. This budget is being depleted by emissions at a rate of 42 ± 3 gigatonnes of CO₂ per year. Putting these two together, scientists predict that the world will overshoot this limit around the beginning of 2028.
+
+The renewable energy percentage is based on the [“Our World In Data”](https://ourworldindata.org/renewable-energy#how-much-of-our-primary-energy-comes-from-renewables) open source database. According to their model, renewable energy provided 11.41% of global energy consumption in 2019. The annual growth is assumed to be 5.655%, which is the average growth rate in renewable energy share from 2016-2019. This rate is continuously updated with new yearly data in the Climate Clock API.
---
+++ climateclockbot.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import config
+import datetime
+import json
+import logging
+import math
+import os
+import random
+import ssl
+import sys
+import time
+import urllib
+from datetime import datetime, timezone
+from datetime import timedelta
+from dateutil.parser import *
+from dateutil.relativedelta import relativedelta
+from twython import Twython
+
+def connectTwitter():
+	# connect to twitter API
+	return Twython(config.twitter_key, config.twitter_secret,
+		config.access_token, config.access_secret)
+
+def post_tweet(twitter, to_tweet):
+	# post the string to twitter
+	twitter.update_status(status=to_tweet)
+
+def getClimateData():
+	# https://climate-clock.gitbook.io/climate-clock-docs/climate-clock-api
+	url = "https://api.climateclock.world/v1/clock"
+	data = json.loads(urllib.request.urlopen(url, context=ssl.SSLContext()).read())
+	return data['data']['modules']
+
+def assembleTweet(data):
+	data_15C = data['carbon_deadline_1']['deadlines']['1.5C']['figures']
+	carbon_budget = data_15C['ipcc_budget_tons_CO2']
+	carbon_per_year = data_15C['ipcc_emissions_tons_CO2_per_year']
+
+	starttime = parse(data_15C['ipcc_budget_start'])
+	endtime = parse(data['carbon_deadline_1']['timestamp'])
+	nowtime = datetime.now(timezone.utc)
+	remaining = relativedelta(endtime, nowtime)
+
+	assert nowtime < endtime, "But... the future refused to change."
+
+	# annoying! extract years + days, ignoring months
+	midtime = nowtime + relativedelta(years=+remaining.years)
+	days_remaining = (endtime - midtime).days
+
+	carbon_per_second = carbon_per_year / timedelta(days=365.2422).total_seconds()
+	budget_remaining = carbon_budget - (nowtime - starttime).total_seconds() * carbon_per_second
+
+	renew_initial = data['renewables_1']['initial']
+	renew_starttime = parse(data['renewables_1']['timestamp'])
+	renew_rate = data['renewables_1']['rate']
+	renew_current = renew_initial + (nowtime - renew_starttime).total_seconds() * renew_rate
+
+	co2_symbols = [u"\U0001F525", u"\U000026A0", u"\U0001F6E2", u"\U000026FD"]
+	format1 = u"%s %.3f Gt of CO%s remaining for +1.5%sC (66%% prob.)"
+	tweet1 = format1 % (random.choice(co2_symbols), (budget_remaining / 1E9), u"\U00002082", u"\U000000B0")
+
+	time_symbols = [u"\U000023F3", u"\U000023F0", u"\U000023F2", u"\U0001F4C5"]
+	format2 = u"%s %i years %i days %i hours until overshoot"
+	tweet2 = format2 % (random.choice(time_symbols), remaining.years, days_remaining, remaining.hours)
+
+	renew_symbols = [u"\U0001F331", u"\U0001F32C", u"\U00002600"]
+	format3 = u"%s %.3f%% of world energy from renewables"
+	tweet3 = format3 % (random.choice(renew_symbols), renew_current)
+
+	return u"%s\n%s\n%s" % (tweet1, tweet2, tweet3)
+	
+if __name__ == "__main__":
+	# heroku scheduler runs every 12 hours
+	try:
+		# assemble climate data
+		data = getClimateData();
+		to_tweet = assembleTweet(data);
+		print(to_tweet.encode('ascii', 'ignore'))
+
+		# post the tweet
+		twitter = connectTwitter()
+		post_tweet(twitter, to_tweet)
+		sys.exit(0) # success!
+	except SystemExit as e:
+		# working as intended, exit normally
+		sys.exit(e)
+	except:
+		logging.exception(sys.exc_info()[0])
